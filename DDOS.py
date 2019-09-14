f163e183ee6de3adea834edecd485cee6e571d11
Aruli <55286556+Aruli-Cyber@users.noreply.github.com>
2019-09-14 09:37:12 +0700

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -78,7 +78,7 @@ def usage():
 	print (''' \033[92m	Aruli-Cyber DDos Script v1
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
-	usage : python3 hammer.py [-s] [-p] [-t]
+	usage : python3 DDOS.py [-s] [-p] [-t]
 	-h : help
 	-s : server ip
 	-p : port default 80
